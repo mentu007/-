dd56fe9668edf43d8bdace6e381480a622c14030
Mentu <2621082160@qq.com>
2023-11-08 15:46:08 +0800

    feature(控制层+业务层): 菜品分类功能
    分类管理下的，新增菜品分类功能实现

---
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -0,0 +1,30 @@
+package com.itheima.reggie.controller;
+
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+@Slf4j
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+        int isAdd = categoryService.save(category);
+        if (isAdd == 1) {
+            return R.success(null, "新增分类成功了");
+        } else {
+            return R.error("新增分类失败了");
+        }
+    }
+
+}
