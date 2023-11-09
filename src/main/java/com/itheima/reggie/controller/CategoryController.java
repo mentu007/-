40a32a87b7b94bb0b634db95ecfbe463f1505422
Mentu <2621082160@qq.com>
2023-11-09 10:19:36 +0800

    feature(控制层→业务层): 分类信息分页查询

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -1,14 +1,12 @@
 package com.itheima.reggie.controller;
 
+import com.itheima.reggie.common.PageResult;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Category;
 import com.itheima.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/category")
@@ -25,6 +23,12 @@ public class CategoryController {
         } else {
             return R.error("新增分类失败了");
         }
+    }
+    @GetMapping("/page")
+    public R<PageResult> list(@RequestParam(defaultValue = "1",required = false) int page,
+                              @RequestParam(defaultValue = "10",required = false) int pageSize){
+        PageResult<Category> pageResult = categoryService.list(page,pageSize);
+        return R.success(pageResult);
     }
 
 }
